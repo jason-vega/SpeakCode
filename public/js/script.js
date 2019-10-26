af3da35d4aa4225f3a4cbb4987e26e79cc8799c5
Jason Vega <jasonvega14@yahoo.com>
2019-10-26 03:37:06 -0700

    Initial design commit

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+var hints = ["Say \"start\" to begin coding", "First time? Say \"teach me\" to learn!"];
+
+function transitionHint(index) {
+  var nextIndex = index + 1 < hints.length ? index + 1 : 0;
+
+  $("#hint").text(hints[index]).fadeTo(1000, 1).delay(3000).fadeTo(1000, 0, function() {
+    transitionHint(nextIndex);
+  }); 
+}
+
+$(document).ready(function () {
+  $("#title").fadeTo(1250, 1, function() {
+    transitionHint(0);
+  });
+});
