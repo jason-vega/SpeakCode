d02dfc4adae100391b61bb7fc703ffe82a99c781
Jason Vega <jasonvega14@yahoo.com>
2019-10-26 10:23:08 -0700

    Moved bucket upload script to index.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,5 @@
 var hints = ["Say \"start\" to begin coding", "First time? Say \"teach me\" to learn!"];
 
-// AWS
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
-var s3 = new AWS.S3();
-var bucketName = 'elasticbeanstalk-us-west-1-116601934919';
-var keyName = 'input/sup.txt';
-var bodyText = "woah";
-var params = {
-  ACL: "authenticated-read",
-  Body: bodyText,
-  Bucket: bucketName,
-  Key: keyName
-};
-
 function transitionHint(index) {
   var nextIndex = index + 1 < hints.length ? index + 1 : 0;
 
@@ -25,14 +11,5 @@ function transitionHint(index) {
 $(document).ready(function () {
   $("#title").fadeTo(1250, 1, function() {
     transitionHint(0);
-  });
-
-  s3.putObject(params, function(err, data) {
-    if (err) { 
-      console.log(err);
-    }
-    else {
-      console.log("SUCCESS");
-    }
   });
 });
